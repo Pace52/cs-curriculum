aadafca3edb6be54111e33adec8a42aca6d3b4a0
Pace52 <plykins4127@student.pps.net>
2024-10-18 14:03:50 -0700

    For some reason it isn't working
    Oh well

--- Assets/Scripts/Package Scripts/Enemy.cs
+++ Assets/Scripts/Package Scripts/Enemy.cs
@@ -1,16 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class EnemyAI : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     public List<GameObject> points;
     public int pointer = 0;
     public GameObject player;
     public States state = States.Wander;
     public Singleton St;
+    private float enemy_cooldown;
+    public Vector3 mtpv;
+    public string direction;
+    public Animator mobileEnemy;
     
     public enum States
     {
@@ -23,28 +28,54 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     private void Start()
     {
-        St = FindObjectofType<Singleton>();
+        St = FindObjectOfType<Singleton>();
     }
     void Update()
     {
+        if (Math.Abs(mtpv.x) > Math.Abs(mtpv.y))
+        {
+            if (mtpv.x > 0)
+            {
+                direction = "Right";
+            }
+
+            if (mtpv.x < 0)
+            {
+                direction = "Left";
+            }
+        }
+        if (Math.Abs(mtpv.y) > Math.Abs(mtpv.x))
+        {
+            if (mtpv.y > 0)
+            {
+                direction = "Up";
+            }
+
+            if (mtpv.y < 0)
+            {
+                direction = "Down";
+            }
+        }
         if (state == States.Wander)
         {
+            mobileEnemy.Play("Walk" + direction);
             Debug.Log("Wandering");
-            if (Vector3.Distance(a: transform.position, b: points[pointer].transform.position) < 0.01f)
+            if (Vector3.Distance(transform.position, points[pointer].transform.position) <= .015)
             {
                 pointer++;
                 pointer %= points.Count;
             }
-            transform.position = Vector3.MoveTowards(current: transform.position, target: points[pointer].transform.position,
-                maxDistanceDelta: 1 * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, points[pointer].transform.position, 1 * Time.deltaTime);
         }
 
         if (state == States.Chase)
         {
+            mobileEnemy.Play("Walk" + direction);
+            mtpv = (player.transform.position - transform.position);
             Debug.Log("Chasing");
             transform.position = Vector3.MoveTowards(current: transform.position, target: player.transform.position,
                 maxDistanceDelta: 1f * Time.deltaTime);
-            if (Vector3.Distance(a: transform.position, b: player.transform.position) < 1f)
+            if (Vector3.Distance(a: transform.position, b: player.transform.position) < 1.3f)
             {
                 state = States.Attack;
             }
@@ -52,7 +83,14 @@ public class EnemyAI : MonoBehaviour
 
         if (state == States.Attack)
         {
+            mobileEnemy.Play("Attack" + direction);
             Debug.Log("Attacking");
+            enemy_cooldown -= Time.deltaTime;
+            if (player && enemy_cooldown < 0)
+            {
+                St.ChangeHealth(cht:-1);
+                enemy_cooldown = 1;
+            }
         }
     }
 
